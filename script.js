0ebb3fef9c12d5a05ab265c1d9c8352a13f31250
salmansaleem2 <salmansaleem8523@gmail.com>
2022-06-19 22:56:12 +0500

    Switching the Active Player functionality added

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 'use strict';
 
 //Selecting Element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score1El = document.querySelector('#score--0');
 const score2El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const current0El = document.querySelector('#current--0');
-const current1El = document.querySelector('#current--1');
 
 // Starting Conditions
 score1El.textContent = 0;
 score2El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling Dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +34,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to the current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
